bb162e2fa9a576b2a1b16a2760132afe8944045f
JaneYWei <janewei.music@gmail.com>
2024-02-25 09:45:24 +1000

    bamble - created rudimentary audio ping system for locating direction of doors. Can try navigating MainGame scene with your eyes closed and see how you go!

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -13,15 +13,18 @@ public class AudioSettings : MonoBehaviour
 
     FMOD.Studio.Bus Music;
     FMOD.Studio.Bus SFX;
+    FMOD.Studio.Bus Accessibility;
     FMOD.Studio.Bus Master;
     float MusicVolume = 0.5f;
     float SFXVolume = 0.5f;
+    float AccessibilityVolume = 0.5f;
     float MasterVolume = 1f;
 
     void Awake()
     {
         Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
         SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        Accessibility = FMODUnity.RuntimeManager.GetBus("bus:/Master/Accessibility");
         Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
         SFXVolumeTestEvent = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/SFXVolumeTest");
     }
@@ -30,6 +33,7 @@ public class AudioSettings : MonoBehaviour
     {
         Music.setVolume(MusicVolume);
         SFX.setVolume(SFXVolume);
+        Accessibility.setVolume(AccessibilityVolume);
         Master.setVolume(MasterVolume);
     }
 
@@ -41,6 +45,11 @@ public class AudioSettings : MonoBehaviour
     public void MusicVolumeLevel(float newMusicVolume)
     {
         MusicVolume = newMusicVolume;
+    }
+
+    public void AccessibilityVolumeLevel(float newAccessibilityVolume)
+    {
+        MusicVolume = newAccessibilityVolume;
     }
 
     public void SFXVolumeLevel(float newSFXVolume)

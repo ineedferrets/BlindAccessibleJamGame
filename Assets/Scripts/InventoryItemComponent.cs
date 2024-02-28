82be1ea9fadda3a05b3e7756907abd26813029cb
Phoebs <m.michael.m.markus@gmail.com>
2024-02-28 11:46:42 +0000

    Changed screen reader to only try on Windows platforms.

--- Assets/Scripts/InventoryItemComponent.cs
+++ Assets/Scripts/InventoryItemComponent.cs
@@ -23,17 +23,21 @@ public class InventoryItemComponent : MonoBehaviour
         CauldronController.AddIngredientOutcome outcome = CauldronController.AddIngredientOutcome.None;
         bool bWasAdded = cauldron.TryAddIngredient(itemAsset, out outcome);
 
-        if (bWasAdded)
+        if (Application.platform != RuntimePlatform.WebGLPlayer)
         {
-            //ScreenReader.StaticReadText(itemAsset.name + " was added to the cauldron.");
+            if (bWasAdded)
+            {
+                ScreenReader.StaticReadText(itemAsset.name + " was added to the cauldron.");
+            }
+            else if (outcome == CauldronController.AddIngredientOutcome.AlreadyContainsIngredient)
+            {
+                ScreenReader.StaticReadText(itemAsset.name + " was already in the pot and has been removed.");
+            }
+            else if (outcome == CauldronController.AddIngredientOutcome.IngredientsFull)
+            {
+                ScreenReader.StaticReadText("The pot is already full.");
+            }
         }
-        else if (outcome == CauldronController.AddIngredientOutcome.AlreadyContainsIngredient)
-        {
-            //ScreenReader.StaticReadText(itemAsset.name + " was already in the pot and has been removed.");
-        }
-        else if (outcome == CauldronController.AddIngredientOutcome.IngredientsFull)
-        {
-            //ScreenReader.StaticReadText("The pot is already full.");
-        }
+
     }
 }
